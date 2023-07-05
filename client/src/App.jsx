bd36bcca6b41bc565dfc261b8ff86b01753d6a39
aryan bramhane <aryanbramhane1@gmail.com>
2023-07-05 22:39:52 +0530

    feat: basic frontend done

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import {BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import Home from './pages/home/index'
+import io from 'socket.io-client'
+import { useState } from 'react'
+
+
+const socket = io.connect('http://localhost:5173')
+function App() {
+  const [username, setUsername] = useState('')
+  const [room, setRoom] = useState('')
+  return (
+    <Router>
+      
+      <div className='App'>
+        <Routes>
+          <Route 
+            path='/' 
+            element={
+              <Home
+                username = {username}
+                setUsername ={setUsername}
+                room = {room}
+                setRoom= {setRoom}
+                socket = {socket}
+              />
+            }/>
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ client/src/pages/home/index.jsx
@@ -0,0 +1,46 @@
+import styles from './styles.module.css'
+import { useNavigate } from 'react-router-dom'
+
+
+
+const Home = ({username, setUsername, room, setRoom, socket}) =>{
+    const navigate = useNavigate()
+
+    const joinRoom = () =>{
+        if(username !=='' && username !== ''){
+            socket.emit('join_room', {username, room})
+            console.log("Room joined")
+        }
+        navigate('/chat', {replace: true})
+    }
+    return(
+        <div className={styles.container}>
+            <div className={styles.formContainer}>
+                <h1>{`<>DEV ROOMS </>`}</h1>
+                <input 
+                    className={styles.input}
+                    placeholder="Username"  
+                    onChange={(e) =>setUsername(e.target.value)}
+                    />
+                <select 
+                    className={styles.input}
+                    onChange={(e) => setRoom(e.target.value)}
+                    >
+                    <option>-- Select Room --</option>
+                    <option value="javascript">Javascript</option>
+                    <option value="node">Node</option>
+                    <option value="express">Express</option>
+                    <option value="react">React</option>
+                </select>
+                <button 
+                    className="btn btn-secondary" 
+                    style={{width: '100%'}}
+                    onClick={joinRoom}
+                    >
+                        Join Room
+                        </button>
+            </div>
+        </div>
+    )
+}
+export default Home
